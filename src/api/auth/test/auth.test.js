cf1eee3d3eacc206b5352606298e4cf3412870ad
Mike Donahue <mtdonahue1@gmail.com>
2017-09-27 23:06:12 -0700

    Start of unit tests

--- src/api/auth/test/auth.test.js
+++ src/api/auth/test/auth.test.js
@@ -1,9 +1,46 @@
-const { assert } = require('chai');
+/* eslint-disable no-unused-expressions */
 
-describe('Array 2', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const authController = require('../auth-controller');
+const User = require('../../../models').user;
+
+describe('Unit Test: Auth', () => {
+  let mock;
+
+  beforeEach(() => {
+    mock = {
+      req: {},
+      res: {
+        status: sinon.spy(() => mock.res),
+        json: sinon.spy(),
+      },
+      model: {
+        findOne: sinon.stub(User, 'findOne'),
+        create: sinon.stub(User, 'create'),
+      },
+    };
+  });
+
+  describe('Signup', () => {
+    it.only('should create a new user if the given email does not exist', () => {
+      const req = {
+        body: {
+          email: 'mike@mail.com',
+          firstName: 'Mike',
+          lastName: 'James',
+          password: 'password',
+          ip: '0.0.0.0',
+        },
+      };
+
+      mock.model.findOne.resolves(null);
+      mock.model.create.resolves({});
+
+      return authController.signup(req, mock.res).then(() => {
+        expect(mock.res.json.called).to.be.true;
+      });
     });
   });
 });
